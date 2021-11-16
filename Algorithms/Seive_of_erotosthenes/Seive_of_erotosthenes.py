94908e2c7b4b710b50c451098fb6f50e37b17498
Jayanth <76648322+jayanth353@users.noreply.github.com>
2021-11-16 18:52:51 +0530

    Create Seive_of_erotosthenes.py

---
+++ Algorithms/Seive_of_erotosthenes/Seive_of_erotosthenes.py
@@ -0,0 +1,11 @@
+
+n=int(input())
+l=[1]*(n+1)
+for i in range(2,int(n**(1/2))+1):
+    
+    if(l[i]==1):
+        for j in range(i**2,n+1,i):
+            l[j]=0
+for k in range(2,len(l)):
+    if l[k]==1 :
+        print(k,end=" ")
