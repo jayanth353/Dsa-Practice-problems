8bdfe407a3c38d1e924f9c35f93d64d6446bba78
Jayanth <76648322+jayanth353@users.noreply.github.com>
2021-11-20 07:37:26 +0530

    Update Prime_factorization.cpp

--- Algorithms/Seive_of_erotosethenes/Prime_factorization.cpp
+++ Algorithms/Seive_of_erotosethenes/Prime_factorization.cpp
@@ -1,56 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5 + 1;
-vector<bool> p(N, true);
-vector<int> lp(N, 0), hp(N, 0);
-
-void seive() //caluculates low prime and high prime for each number upto N()
+void prime_factorization(int n)
 {
-
-	p[1] = false;
-	p[0] = false;
-
-	//seive algorithm
-	for (int i = 2; i <= N; ++i)
+	vector<int> p_factors;
+	for (int i = 2; i <= sqrt(n); ++i) //refer prime.md file
 	{
 
-		if (p[i] == true)
+		while (n % i == 0)
 		{
-			lp[i] = i;
-			hp[i] = i;
-			for (int j = 2 * i; j <= N; j += i)
-			{
-				p[j] = false;
-				hp[j] = i;
-				if (lp[j] == 0) lp[j] = i;
-
-			}
+			p_factors.push_back(i);
+			n /= i;
 		}
 	}
-	// cout << "low_prime" << "\t" << "high_prime" << endl;
 
-	// for (int j = 2; j <= n; ++j)
-	// {
-
-	// 	cout << "\t" << lp[j] << "\t \t \t" << hp[j] << "\n";
-	// }
+	if (n > 1) p_factors.push_back(n);//if n is prime number
+	for (int f : p_factors)
+		cout << f << " ";
 }
 
 int main()
 {
 	int n;
-	cin >> n;//n value should be less than N(line 4)
-	seive();
-	vector<int> Pfactorisation;
-	while (n > 1)
-	{
-		int Pfactor = lp[n];
-		while (n % Pfactor == 0)
-		{
-			Pfactorisation.push_back(Pfactor);
-			n /= Pfactor;
-		}
-	}
-	for (int p : Pfactorisation) cout << p << " ";
+	cin >> n;// n is a number for which prime factorisation is calculated
+	prime_factorization(237584); //
+
 }
